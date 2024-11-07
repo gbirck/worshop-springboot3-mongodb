910d8f06846db9e3ce28e9e063b8d6fd06b4aeef
Gabriel Birck <birckzin@gmail.com>
2024-11-07 17:18:53 -0300

    Adicionado padrao dto pra adicionar usuarios

--- src/main/java/com/coursemongodb/workshopmongo/resources/UserResource.java
+++ src/main/java/com/coursemongodb/workshopmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.coursemongodb.workshopmongo.resources;
 
+import com.coursemongodb.workshopmongo.DTO.UserDTO;
 import com.coursemongodb.workshopmongo.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.coursemongodb.workshopmongo.services.UserService;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -18,8 +20,9 @@ public class UserResource {
     private UserService service;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll() {
+    public ResponseEntity<List<UserDTO>> findAll() {
         List<User> list = service.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> dtos = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(dtos);
     }
 }
